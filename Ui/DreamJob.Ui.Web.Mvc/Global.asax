a1777e4ed4c1fdbf6ffc4e08ec7a3b1ec7e2bbae
jstadnicki <jaroslaw.stadnicki@gmail.com>
2014-07-16 23:59:16 +0200

    Lets call it an initial upload

---
+++ Ui/DreamJob.Ui.Web.Mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DreamJob.Ui.Web.Mvc.MvcApplication" Language="C#" %>
---
+++ Ui/DreamJob.Ui.Web.Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DreamJob.Ui.Web.Mvc.Startup))]
+namespace DreamJob.Ui.Web.Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
