0c82df4268debc19c4b091af376ff32fc7ac5633
ibezuglyi <ievgen.bezuglyi@gmail.com>
2014-10-08 18:32:26 +0200

    escaping html tags

--- Ui/DreamJob.Ui.Web.Mvc/AngularApplication/Controllers/DevelopersSearchController.js
+++ Ui/DreamJob.Ui.Web.Mvc/AngularApplication/Controllers/DevelopersSearchController.js
@@ -2,10 +2,12 @@
     $scope.searchString = "";
     $scope.searchCity = "";
     $scope.developers = [];
+    var htmlCleanerRxp = /(<([^>]+)>)/ig;
 
     $scope.searchForDevelopers = function () {
+
         djClientApi
-            .searchProfile($scope.searchString, $scope.searchCity)
+            .searchProfile($scope.searchString.replace(htmlCleanerRxp,''), $scope.searchCity.replace(htmlCleanerRxp,''))
             .then(function (resp) {
                 $scope.developers = resp.data;
             });
