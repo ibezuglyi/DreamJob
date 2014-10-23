ad9bc146cc1fed2cc052cc224f397468356c2527
jstadnicki <jaroslaw.stadnicki@gmail.com>
2014-10-24 00:22:13 +0200

    search working again

--- Ui/DreamJob.Ui.Web.Mvc/AngularApplication/Controllers/DevelopersSearchController.js
+++ Ui/DreamJob.Ui.Web.Mvc/AngularApplication/Controllers/DevelopersSearchController.js
@@ -1,26 +1,24 @@
 ﻿angular
     .module('djapp')
-    .controller('DevelopersSearchController', function($scope, $http, djClientApi) {
+    .controller('DevelopersSearchController', function ($scope, $http, djClientApi) {
         $scope.searchString = "";
         $scope.searchCity = "";
         $scope.developers = [];
         var htmlCleanerRxp = /(<([^>]+)>)/ig;
 
-        $scope.searchForDevelopers = function() {
+        $scope.searchForDevelopers = function () {
 
             djClientApi
                 .searchProfile($scope.searchString.replace(htmlCleanerRxp, ''), $scope.searchCity.replace(htmlCleanerRxp, ''))
-                .then(function(resp) {
+                .then(function (resp) {
                     $scope.developers = resp.data;
                 });
         };
 
-        $scope.getCities = function() {
-            $scope.getCities = function(val) {
-                return djClientApi.getCities(val)
-                    .then(function(resp) {
-                        return resp.data;
-                    });
-            };
+        $scope.getCities = function (val) {
+            return djClientApi.getCities(val)
+                .then(function (resp) {
+                    return resp.data;
+                });
         };
     });
